0c22d3cd7e588f2c7d32a33b0fdeca01b10aac95
Emily <emily.zhao23@gmail.com>
2024-10-03 11:59:09 -0400

    counter using signals

--- frontend/src/app/labs/labs.routes.ts
+++ frontend/src/app/labs/labs.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { LabsComponent } from './labs.component';
+import { CounterComponent } from '../components/counter.component';
 
 export const LABS_ROUTES: Routes = [
   {
     path: '',
     component: LabsComponent,
+    children: [
+      {
+        path: 'counter',
+        component: CounterComponent,
+      }
+    ]
   },
 ];
