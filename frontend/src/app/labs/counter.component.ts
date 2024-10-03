19ce661d81c978266107730a8d12ac8ee6d61f0a
Brett Alessandri <balessandri@live.com>
2024-10-03 12:21:07 -0400

    Sprint 2.5

--- frontend/src/app/labs/counter.component.ts
+++ frontend/src/app/labs/counter.component.ts
@@ -7,15 +7,22 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
   imports: [],
   template: `
     <p>Counter: {{ counter() }}</p>
-    <div class="flex gap-4">
-      <button
-        class="btn btn-warning"
-        (click)="onDecrement()"
-        [disabled]="counter() === 0"
-      >
-        -
-      </button>
-      <button class="btn btn-primary" (click)="onIncrement()">+</button>
+    <div class="flex flex-col">
+      <div class="flex gap-4">
+        <button
+          class="btn btn-warning"
+          (click)="onDecrement()"
+          [disabled]="counter() === 0"
+        >
+          -
+        </button>
+        <button class="btn btn-primary" (click)="onIncrement()">+</button>
+      </div>
+      <div>
+        <p [hidden]="counter()! % 3">Fizz</p>
+        <p [hidden]="counter()! % 3 || counter() % 5">FizzBuzz</p>
+        <p [hidden]="counter()! % 5">Buzz</p>
+      </div>
     </div>
   `,
   styles: ``,
